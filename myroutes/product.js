6b8824e563d78dfb6991a6ed5ae882b47af0260b
Prince Bassey <basseyprince75@gmail.com>
2020-07-20 14:40:02 +0100

    updated

--- myroutes/product.js
+++ myroutes/product.js
@@ -1,20 +1,18 @@
-const express = require('express')
+const express = require("express");
 
-const adminController = require('../controllers/admin')
+const adminController = require("../controllers/admin");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/add-product', adminController.addProducts)
+router.get("/add-product", adminController.addProducts);
 
-router.post('/add-product',adminController.postProduct)
+router.post("/add-product", adminController.postProduct);
 
-router.get('/admin-product-list' , adminController.getProduct)
+router.get("/admin-product-list", adminController.getProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct)
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
-
-
-module.exports = router
+module.exports = router;
 
 //  exports.routes = router
 //  exports.products = products
